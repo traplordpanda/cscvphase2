dca602ceedf14f5166e8a475e4abf2906cfc7e5a
Kyle AuBuchon <kdaub.tf2@gmail.com>
2022-03-23 15:47:12 -0400

    first update

--- Phase 2/phase2.c
+++ Phase 2/phase2.c
@@ -1,4 +1,5 @@
 /* ------------------------------------------------------------------------
+   Kyle AuBuchon & Juan Gonzalez
    phase2.c
    Applied Technology
    College of Applied Science and Technology
@@ -17,6 +18,25 @@
 int start1 (char *);
 extern int start2 (char *);
 
+void check_kernel_mode(char *message);
+void debugConsole(char *message);
+static void enableInterrupts();
+static void disableInterrupts();
+
+int 		check_io();
+
+void		zeroMailbox(int mailbox_ID);
+void		zeroSlot(int slot_ID);
+void		zeroMboxSlot(int pid);
+
+
+int			MboxRelease(int mailboxID);
+int			MboxCondSend(int mailboxID, void *msg, int msg_Size);
+int			MboxCondReceive(int mailboxID, void *msg, int max_msg_Size);
+
+slot_ptr    createSlot(int index, int mbox_ID, void *msg, int msg_Size);
+int			getSlot();
+//int			addSlotToList(slot_ptr slot,  mailbox_ptr);
 
 /* -------------------------- Globals ------------------------------------- */
 
@@ -49,23 +69,22 @@ int start1(char *arg)
 {
    int kid_pid, status; 
 
-   if (DEBUG2 && debugflag2)
-      console("start1(): at beginning\n");
-
-   //check_kernel_mode("start1");
+   debugConsole("start1(): at beginning\n");
+   check_kernel_mode("start1");
 
    /* Disable interrupts */
-   //disableInterrupts();
+   disableInterrupts();
 
    /* Initialize the mail box table, slots, & other data structures.
     * Initialize int_vec and sys_vec, allocate mailboxes for interrupt
     * handlers.  Etc... */
 
-   //enableInterrupts();
+   /* Enable interrupts */
+   enableInterrupts();
 
    /* Create a process for start2, then block on a join until start2 quits */
-   if (DEBUG2 && debugflag2)
-      console("start1(): fork'ing start2 process\n");
+   debugConsole("start1(): fork'ing start2 process\n");
+   
    kid_pid = fork1("start2", start2, NULL, 4 * USLOSS_MIN_STACK, 1);
    if ( join(&status) != kid_pid ) {
       console("start2(): join returned something other than start2's pid\n");
@@ -119,3 +138,34 @@ int MboxReceive(int mbox_id, void *msg_ptr, int msg_size)
 int check_io(){
     return 0; 
 }
+
+//convenience function to check kernel mode 
+void check_kernel_mode(char *message) {
+  if ((PSR_CURRENT_MODE & psr_get()) == 0) {
+    console("Kernel mode check fail - %s\n", message);
+    halt(1);
+  }
+}
+
+//convenience function for debug output with USLOSS
+void debugConsole(char *message){
+   if (DEBUG2 && debugflag2)
+      console(message);
+}
+
+//Disables the interrupts.
+void disableInterrupts(void) {
+  /* check kernel mode */
+  check_kernel_mode("disable interrupt function");
+
+  /* perform bitwise to set disable */
+  psr_set(psr_get() & ~PSR_CURRENT_INT);
+} /* disableInterrupts */
+
+//Enables the interrupts.
+void enableInterrupts(void) {
+  /* Turn the interrupts ON if we are in kernel mode */
+  check_kernel_mode("enable interrupt function");
+
+  psr_set(psr_get() | PSR_CURRENT_INT);
+} /*enableInterrupts*/
---
+++ Phase 2/scratch.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <phase1.h>
+#include <phase2.h>
+#include <usloss.h>
+#include "message.h"
+
+int main(){
+    printf("this is an enviroment test s");
+}
